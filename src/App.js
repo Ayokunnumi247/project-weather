e27c4ae87e87d55b3f49c9879e921d24ecc2c927
Ayokunnumi247 <163436878+Ayokunnumi247@users.noreply.github.com>
2024-09-27 12:46:40 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./index.css";
 
 function App() {
-  return <div>new york</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <p>
+          The project was code {""}
+          <a href="https://github.com/Ayokunnumi247">Ayokunnumi Andu</a>
+          {""} and is {""}
+          <a href="https://github.com/Ayokunnumi247/project-weather">
+            open-sourced on GitHub.
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
