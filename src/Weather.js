61668baed0f6fe38dde42a927f507a10724a5a80
Ayokunnumi247 <163436878+Ayokunnumi247@users.noreply.github.com>
2024-11-07 19:59:31 +0000

    updated correct URL

--- src/Weather.js
+++ src/Weather.js
@@ -15,8 +15,7 @@ export default function Weather(props) {
       humidity: response.data.temperature.humidity,
       wind: response.data.wind.speed,
       description: response.data.condition.description,
-      iconUrl:
-        "http://shecodes-assets.s3.amazonaws.com/api/weather/icons/scattered-clouds-day.png",
+      iconUrl: `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`,
       date: response.data.time * 1000,
       city: response.data.city,
     });
