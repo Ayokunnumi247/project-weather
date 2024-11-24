64110bbda6c66b9e8797ac9eea299de99d83aeb2
Ayokunnumi247 <163436878+Ayokunnumi247@users.noreply.github.com>
2024-11-24 20:46:43 +0000

    forecast days of the week

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -6,15 +6,18 @@ export default function WeatherForecastDay(props) {
     let temperature = Math.round(props.data.temperature.maximum);
     return `${temperature}°`;
   }
+
   function minTemperature() {
     let temperature = Math.round(props.data.temperature.minimum);
     return `${temperature}°`;
   }
+
   function day() {
-    let date = new Date(props.data.temperature.day * 1000);
+    let date = new Date(props.data.time * 1000);
     let day = date.getDay();
 
     let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
     return days[day];
   }
 
